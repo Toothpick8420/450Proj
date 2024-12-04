16245c403d8cc404873f2586e678baac1b1adf36
oakleafknight1 <oakleafknightwon@gmail.com>
2024-12-03 20:08:27 -0800

    added move back from before we deleted it

--- dumb/main.py
+++ dumb/main.py
@@ -49,6 +49,36 @@ class AI:
                         #else:
                         #    print("r:",r0,"c:",c0)
         return ret
+    def move(b, p):
+        #b is board, p is player
+        #changes board and returns void
+        vm = valid_moves(b, p)
+        if vm == []: #When no valid moves
+            return
+        if p == computer:
+            (r, c) = vm[0] #when computer is playing, plays first detected move
+        else:
+            r = int(input("row: "))#asks player for move... should move this out
+            c = int(input("col: "))
+            #going to assume they moved correctly...
+        b[r][c] = p
+        for (dr, dc) in [(+0, +1), (-1, +1), (-1, +0), (-1, -1),
+                         (+0, -1), (+1, -1), (+1, +0), (+1, +1)]:
+            r0, c0 = r + dr, c + dc
+            flag = False #to make sure it goes through while loop
+            while 0 <= r0 < n and 0 <= c0 < n and \
+                  b[r0][c0] != free and b[r0][c0] != p: 
+                #detected valid direction, continue until free spot
+                r0, c0 = r0 + dr, c0 + dc
+                flag = True
+                if 0 <= r0 < n and 0 <= c0 < n and b[r0][c0] == p and flag:
+                    print("between rows",r,r0, "between col",c,c0)
+                    r1, c1 = r, c
+                    while r1 != r0 or c1 != c0:
+                        print("r1:",r1,"c1:",c1)
+                        b[r1][c1] = p
+                        r1, c1 = r1 + dr, c1 + dc
+        return
  
     
 class Rando(AI):
